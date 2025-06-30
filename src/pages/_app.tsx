289f35e1794a0d5c623b8df0a3a184b9c3dc925e
build-africa-eng <afrcanfuture@gmail.com>
2025-07-01 01:37:31 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-// src/pages/_app.tsx
-import '@/app/globals.css'; 
+import type { AppProps } from 'next/app';
+import '@/app/globals.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
